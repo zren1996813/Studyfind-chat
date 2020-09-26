3311a715b06e6110080ccda474db48c126740d2f
Ziyao Ren <zren1996813@hotmail.com>
2020-09-26 16:31:08 -0400

    did the signup [age

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   Route, 
   BrowserRouter as Router,
   Switch,
-  Redirect
 } from  'react-router-dom';
 import './App.css'
 import Home from './Pages/Home/Home';
@@ -11,7 +10,6 @@ import Chat from './Pages/Chat/Chat';
 import Profile from './Pages/Profile/Profile';
 import Signup from './Pages/Signup/Signup';
 import Login from './Pages/Login/Login';
-import firebase from './Services/firebase';
 import {toast, ToastContainer} from 'react-toastify';
 
 class App extends Component{
@@ -30,32 +28,10 @@ class App extends Component{
 
   constructor() {
     super();
-    this.state = {
-      authenticated: false,
-      loading: true
-    };
   }
-  componentDidMount(){
-    firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        this.setState({
-          authenticated: true,
-          laoding: false
-        });
-      } else {
-        this.setState({
-          authenticated: false,
-          loading:false
-        });
-      }
-    })
-  }
+  
   render(){
-    return this.state.loading ===true ? (
-      <div className="spinner-border text-success" role = 'states'>
-        <span className = "sr-only"> Loading....</span>
-      </div>
-    ): (
+    return (
       <Router>
         <ToastContainer
           autoClose = {2000}
---
+++ src/Pages/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Chat extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+    render(){
+        return (
+            <div>
+                this is a chat 
+            </div>
+        )
+    }
+}
