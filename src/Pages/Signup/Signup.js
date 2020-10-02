282bba0de529d5851ce11df24ae6520f154bfcfb
Ziyao Ren <zren1996813@hotmail.com>
2020-10-02 19:50:05 -0400

    modify the login according to the login2 in data-handling branch

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,142 +1,98 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, Component} from 'react';
 import "./Signup.css";
 import firebase from "../../Services/firebase";
-import {Card} from 'react-bootstrap'
+import {Card} from 'react-bootstrap';
+import {
+    Link
+  } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import LoginString from '../Login/LoginString';
 import Header from '../../Components/Header';
-export default class Signup extends Component{
+export default function Signup({history}){
 
-    state = {
-            email:"",
-            password:"",
-            name:"",
-            error:null
-    }
+    const [inputs,setInputs] = useState({
+        email: '',
+        password: '',
+        name: '',
+    });
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
+    const handleInput = event => {
+        setInputs({...inputs, [event.target.id]: event.target.value});
     }
-    handleSubmit = async (event) => {
-        const {name,password,email} = this.state;
-        event.preventDefault();
-        try{
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(async result =>{
+    const handleSubmit = event => {
+        firebase.auth().createUserWithEmailAndPassword(inputs.email, inputs.password)
+            .then(data => {
+                alert("Sign up successful!");
+                const uid = data.user.uid;
                 firebase.firestore().collection('users')
                 .add({
-                    name,
-                    id:result.user.uid,
+                    name: inputs.name,
+                    id: uid,
                     URL:'',
                     messages:[{notificationId:"", number:0}]
                 }).then((docRef)=>{
-                    this.setState({
-                        name:'',
-                        password:'',
-                        url:'',
-                    });
-                    this.props.history.push("/chat")
+                    history.push("/chat")
                 })
                 .catch((error)=>{
-                    console.error("Error adding document",error)
+                    console.error("Error adding document",error);
+                    console.log(error);
                 })
             })
-        }
-        // Todo: the error parts
-        catch(error){
-            this.setState({
-                error: "error"
+            .catch(error => {
+                alert("Eroor occured: " + error.message);
             })
-        }
     }
-    render(){
-        const Signinsee = {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            color: 'White',
-            backgroundColor: '#1ebea5',
-            width: '100%',
-            boxShadow: '0 5px 5px #808888',
-            height: '10rem',
-            paddingTop: '48px',
-            opacity: '0.5',
-            borderBottom: '5px solid green',
-        }
 
-        return(
-            
-            <div>
-                <Header/>
-                <CssBaseline/>
-                <Card className='formacontrooutside'>
-                    <form className = "customform" noValidate onSubmit={this.handleSubmit}>
+   
+    return(
+        <div>
+            <Header/>
+            <CssBaseline/>
+            <Card className='formacontrooutside'>
+                <form className = "customform">
                         
-                        <TextField
-                        variant='outlined'
-                        margin='normal'
-                        required
-                        fullWidth
+                <div>
+                    <label className='signUpText'> Email: </label>
+                        <input
                         id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        onChange={this.handleChange}
-                        value={this.state.email}
-                        />  
-                    
-                    <TextField
-                        variant='outlined'
-                        margin='normal'
-                        required
-                        fullWidth
+                        type="text"
+                        value={inputs.email}
+                        onChange={handleInput}
+                        />
+                </div>
+                <div>
+                    <label className='passwordField'> Password: </label>
+                        <input
                         id="password"
-                        label="Password"
-                        name="password"
                         type="password"
-                        autoComplete="current-password"
-                        autoFocus
-                        onChange={this.handleChange}
-                        value={this.state.password}
-                    />
-                    <TextField
-                        variant='outlined'
-                        margin='normal'
-                        required
-                        fullWidth
+                        value={inputs.password}
+                        onChange={handleInput}
+                        />
+                </div>
+                <div>
+                    <label className='signUpText'> Name: </label>
+                        <input
                         id="name"
-                        label="Your Name"
-                        name="name"
-                        autoComplete="name"
-                        autoFocus
-                        onChange={this.handleChange}
-                        value={this.state.name}
-                    />
-                    <div className="CenterAlignItems">
-                        <button class="button1" type="submit">
-                            <span>Sign Up</span>
-                        </button>
-                    </div>
-                    <div>
-                        <p style={{color:'grey'}}>Already have an account?</p>
-                        <Link to="/login">
-                            Log In
-                        </Link>
-                    </div>
-                    <div className="error">
-                        <p id='1' style={{color:'red'}}>
-
-                        </p>
-                    </div>
-                    </form> 
-                </Card>
-            </div>
-        )
-    }
+                        type="text"
+                        value={inputs.name}
+                        onChange={handleInput}
+                        />
+                </div>
+                <div className="CenterAlignItems">
+                    <button onClick={handleSubmit}>
+                        <span>Sign Up</span>
+                    </button>
+                </div>
+                <div>
+                    <p style={{color:'grey'}}>Already have an account?</p>
+                    <Link to="/login">
+                        Log In
+                    </Link>
+                </div>
+                </form> 
+            </Card>
+        </div>
+    )
 }
